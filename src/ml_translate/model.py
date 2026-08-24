39e8011c7b1257a56d8e339bce8176f3bf16dc33
William Woodhead <6097010+nofable@users.noreply.github.com>
2026-08-24 07:05:31 +0100

    Reordered and tidied up the model file

--- src/ml_translate/model.py
+++ src/ml_translate/model.py
@@ -13,77 +13,28 @@ def scaled_dot_product_attention(q, k, v, mask=None):
     return inter @ v
 
 
-class Norm(nn.Module):
-    def __init__(self, d_model, eps=1e-6):
+class Transformer(nn.Module):
+    def __init__(self, d_model, n_layers, ff_d_hidden):
         super().__init__()
-        # parameters shared across positions
-        self.gamma = nn.Parameter(torch.ones(d_model))
-        self.beta = nn.Parameter(torch.zeros(d_model))
-        self.eps = eps
+        self.encoder = Encoder(
+            d_model=d_model, n_layers=n_layers, ff_d_hidden=ff_d_hidden
+        )
+        self.decoder = Decoder(
+            d_model=d_model, n_layers=n_layers, ff_d_hidden=ff_d_hidden
+        )
 
-    def forward(self, input):
-        mean = torch.mean(input, dim=-1, keepdim=True)
-        var = torch.var(input, dim=-1, keepdim=True)
-        return self.gamma * (input - mean) / torch.sqrt(var + self.eps) + self.beta
+    def encode(self, input):
+        self.encoder.forward(input)
 
+    def decode(self, input, encoder_output):
+        self.decoder.forward(input, encoder_output)
 
-class FeedForward(nn.Module):
-    def __init__(self, d_model, d_hidden=2048):
-        super().__init__()
-        # parameters shared across positions
-        self.w1 = nn.Parameter(torch.ones((d_model, d_hidden)))
-        self.b1 = nn.Parameter(torch.zeros(d_hidden))
-        self.w2 = nn.Parameter(torch.ones((d_hidden, d_model)))
-        self.b2 = nn.Parameter(torch.zeros(d_model))
-
-    def forward(self, input):
-        ReLU = nn.ReLU()
-        return ReLU(input @ self.w1 + self.b1) @ self.w2 + self.b2
-
-
-class AddAndNorm(nn.Module):
-    def __init__(self, d_model, eps=1e-6):
-        super().__init__()
-        self.norm = Norm(d_model=d_model, eps=eps)
-
-    def forward(self, input, sublayer_output):
-        # diverging from paper for better gradient flow
-        # paper gives LayerNorm(x + Sublayer(x))
-        return input + self.norm.forward(sublayer_output)
-
-
-class SingleHeadAttention(nn.Module):
-    def __init__(self, d_in, d_out, mask=None):
-        super().__init__()
-        self.v_linear = nn.Linear(in_features=d_in, out_features=d_out)
-        self.k_linear = nn.Linear(in_features=d_in, out_features=d_out)
-        self.q_linear = nn.Linear(in_features=d_in, out_features=d_out)
-        self.mask = mask
-
-    def forward(self, v, k, q):
-        l_v = self.v_linear.forward(v)
-        l_k = self.k_linear.forward(k)
-        l_q = self.q_linear.forward(q)
-        return scaled_dot_product_attention(l_q, l_k, l_v, self.mask)
-
-
-class MultiHeadAttention(nn.Module):
-    # h is number of heads
-    def __init__(self, n_heads, d_model, mask=None):
-        super().__init__()
-        self.n_heads = n_heads
-        # ensure this is a neat split
-        assert d_model % n_heads == 0
-        self.heads = [
-            SingleHeadAttention(d_in=d_model, d_out=d_model / n_heads, mask=mask)
-            for _ in range(n_heads)
-        ]
-        self.out_linear = nn.Linear(d_model, d_model)
-
-    def forward(self, v, k, q):
-        outputs = [head.forward(v, k, q) for head in self.heads]
-        x = torch.concat(outputs)
-        return self.out_linear.forward(x)
+    def transform(self, input):
+        encoder_output = self.encode(input)
+        # TODO: need to make sure the decoder input is correct
+        decoder_output = self.decode(input, encoder_output)
+        # TODO: need to do final linear projection to embedding dictionary size and softmax
+        return decoder_output
 
 
 class Encoder(nn.Module):
@@ -145,13 +96,13 @@ class DecoderLayer(nn.Module):
         )
 
     def forward(self, input, encoder_output):
-        # need to do the right shift
         x = input
         x = self.maskedMultiHeadSublayer.forward(x)
         x = self.multiHeadSublayer.forward(
             x, override_k=encoder_output, override_v=encoder_output
         )
-        x = self.feedForwardSublayer
+        x = self.feedForwardSublayer.forward(x)
+        return x
 
 
 class MultiHeadSublayer(nn.Module):
@@ -163,9 +114,9 @@ class MultiHeadSublayer(nn.Module):
         self.addAndNorm = AddAndNorm(d_model=d_model)
 
     def forward(self, input, override_k=None, override_v=None):
-        k = override_k or input.clone()
-        v = override_v or input.clone()
-        q = input.clone()
+        k = override_k or input
+        v = override_v or input
+        q = input
         x = self.multiHeadAttention.forward(v, k, q)
         x = self.addAndNorm.forward(input, x)
         return x
@@ -184,25 +135,74 @@ class FeedForwardSublayer(nn.Module):
         return x
 
 
-class Transformer(nn.Module):
-    # size is 2-dim, first is sequence len, second is embedding size 512
-    def __init__(self, d_model, n_layers, ff_d_hidden):
+class AddAndNorm(nn.Module):
+    def __init__(self, d_model, eps=1e-6):
         super().__init__()
-        self.encoder = Encoder(
-            d_model=d_model, n_layers=n_layers, ff_d_hidden=ff_d_hidden
-        )
-        self.decoder = Decoder(
-            d_model=d_model, n_layers=n_layers, ff_d_hidden=ff_d_hidden
-        )
+        self.norm = Norm(d_model=d_model, eps=eps)
 
-    def encode(self, input):
-        self.encoder.forward(input)
+    def forward(self, input, sublayer_output):
+        # diverging from paper for better gradient flow
+        # paper gives LayerNorm(x + Sublayer(x))
+        return input + self.norm.forward(sublayer_output)
 
-    def decode(self, input, encoder_output):
-        self.decoder.forward(input, encoder_output)
 
-    def transform(self, input):
-        encoder_output = self.encode(input)
-        decoder_output = self.decode(input, encoder_output)
-        # need to do final linear projection to embedding dictionary size and softmax
-        return decoder_output
+class Norm(nn.Module):
+    def __init__(self, d_model, eps=1e-6):
+        super().__init__()
+        # parameters shared across positions
+        self.gamma = nn.Parameter(torch.ones(d_model))
+        self.beta = nn.Parameter(torch.zeros(d_model))
+        self.eps = eps
+
+    def forward(self, input):
+        mean = torch.mean(input, dim=-1, keepdim=True)
+        var = torch.var(input, dim=-1, keepdim=True)
+        return self.gamma * (input - mean) / torch.sqrt(var + self.eps) + self.beta
+
+
+class FeedForward(nn.Module):
+    def __init__(self, d_model, d_hidden):
+        super().__init__()
+        # parameters shared across positions
+        self.w1 = nn.Parameter(torch.ones((d_model, d_hidden)))
+        self.b1 = nn.Parameter(torch.zeros(d_hidden))
+        self.w2 = nn.Parameter(torch.ones((d_hidden, d_model)))
+        self.b2 = nn.Parameter(torch.zeros(d_model))
+
+    def forward(self, input):
+        ReLU = nn.ReLU()
+        return ReLU(input @ self.w1 + self.b1) @ self.w2 + self.b2
+
+
+class MultiHeadAttention(nn.Module):
+    # h is number of heads
+    def __init__(self, n_heads, d_model, mask=None):
+        super().__init__()
+        self.n_heads = n_heads
+        # ensure this is a neat split
+        assert d_model % n_heads == 0
+        self.heads = [
+            SingleHeadAttention(d_in=d_model, d_out=d_model / n_heads, mask=mask)
+            for _ in range(n_heads)
+        ]
+        self.out_linear = nn.Linear(d_model, d_model)
+
+    def forward(self, v, k, q):
+        outputs = [head.forward(v, k, q) for head in self.heads]
+        x = torch.concat(outputs)
+        return self.out_linear.forward(x)
+
+
+class SingleHeadAttention(nn.Module):
+    def __init__(self, d_in, d_out, mask=None):
+        super().__init__()
+        self.v_linear = nn.Linear(in_features=d_in, out_features=d_out)
+        self.k_linear = nn.Linear(in_features=d_in, out_features=d_out)
+        self.q_linear = nn.Linear(in_features=d_in, out_features=d_out)
+        self.mask = mask
+
+    def forward(self, v, k, q):
+        l_v = self.v_linear.forward(v)
+        l_k = self.k_linear.forward(k)
+        l_q = self.q_linear.forward(q)
+        return scaled_dot_product_attention(l_q, l_k, l_v, self.mask)
