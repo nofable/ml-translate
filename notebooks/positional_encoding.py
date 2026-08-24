25b44693abdddec721b57c1ed254a35c50fb360f
William Woodhead <6097010+nofable@users.noreply.github.com>
2026-08-24 21:41:50 +0100

    visualised positional encoding with altair in jupyter lab

--- notebooks/positional_encoding.py
+++ notebooks/positional_encoding.py
@@ -1,8 +1,28 @@
 # %% [markdown]
-# # Quarterly Sales
-# A look at Q4 revenue by region.
+# Positional Encoding
 
 # %%
-print("hello")
+import torch
+import pandas as pd
+import altair as alt
+from ml_translate.model import PositionalEncoder
 
+pe = PositionalEncoder(20, 100)
+output = pe.forward(torch.zeros(size=(100, 20)))
+data = pd.concat(
+    [
+        pd.DataFrame(
+            {
+                "embedding": output[:, dim],
+                "dimension": dim,
+                "position": list(range(100)),
+            }
+        )
+        for dim in [5, 6, 7, 8]
+    ]
+)
+
+alt.Chart(data).mark_line().properties(width=800).encode(
+    x="position", y="embedding", color="dimension:N"
+).interactive()
 # %%
