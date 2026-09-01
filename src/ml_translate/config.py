259cfbf617076b018b26b30fa016c59101a7209b
William Woodhead <6097010+nofable@users.noreply.github.com>
2026-09-01 07:05:11 +0100

    dynamic optimizer scheduler

--- src/ml_translate/config.py
+++ src/ml_translate/config.py
@@ -4,3 +4,10 @@ EOS_TOKEN = "[eos]"
 
 TOKENIZER_FILE = "byte-level-bpe.tokenizer.json"
 ENG_FRA_TEXT_FILE = "data/eng-fra.txt"
+ENG_FRA_VOCAB_SIZE = 32_000
+
+ENG_GER_VOCAB_SIZE = 37_000
+
+D_MODEL = 512
+
+WARMUP_STEPS = 4000
--- src/ml_translate/train.py
+++ src/ml_translate/train.py
@@ -3,21 +3,27 @@ from torch import Tensor
 from torch.nn import CrossEntropyLoss
 
 from ml_translate.collate import collate
-from ml_translate.config import ENG_FRA_TEXT_FILE, PAD_TOKEN
+from ml_translate.config import (
+    D_MODEL,
+    ENG_FRA_TEXT_FILE,
+    ENG_FRA_VOCAB_SIZE,
+    PAD_TOKEN,
+    WARMUP_STEPS,
+)
 from ml_translate.data_loader import TranslateDataLoader
 from ml_translate.tokenizer import BytePairTokenizer
 from ml_translate.transformer import Transformer
 
 
-num_epochs = 2
+num_epochs = 10
 
 bytePairTokenizer = BytePairTokenizer(src_file=ENG_FRA_TEXT_FILE)
 tokenizer = bytePairTokenizer.getTokenizer()
 
 model = Transformer(
-    d_model=512,
+    d_model=D_MODEL,
     max_seq_len=10_000,
-    num_embeddings=10_000,
+    num_embeddings=ENG_FRA_VOCAB_SIZE,
     n_layers=6,
     ff_d_hidden=2048,
     p_dropout=0.1,
@@ -31,6 +37,13 @@ loss_fn: CrossEntropyLoss = torch.nn.CrossEntropyLoss(
 )
 
 optim = torch.optim.AdamW(model.parameters(), lr=0.5, betas=(0.9, 0.98))
+
+scheduler = torch.optim.lr_scheduler.LambdaLR(
+    optim,
+    lr_lambda=lambda step: (
+        D_MODEL**-0.5 * min((step + 1) ** -0.5, (step + 1) * WARMUP_STEPS**-1.5)
+    ),
+)
 
 dataloader = TranslateDataLoader()
 train_dataloader, test_dataloader = dataloader.train_test_dataloaders()
@@ -52,11 +65,12 @@ def run_batch(model, tokenizer, x, y) -> tuple[Tensor, Tensor]:
     return logits, expected_result
 
 
-for epoch in range(num_epochs):
+for epoch in range(1, num_epochs + 1):
     model.train()
 
     for train_x, train_y in train_dataloader:
         optim.zero_grad()
+
         logits, expected_result = run_batch(model, tokenizer, train_x, train_y)
         loss = loss_fn(
             logits.reshape(-1, logits.size(-1)),
@@ -64,6 +78,7 @@ for epoch in range(num_epochs):
         )
         loss.backward()
         optim.step()
+        scheduler.step()
 
     model.eval()
     total_loss = 0
